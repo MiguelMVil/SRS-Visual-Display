acc9b8b0fc3e68ef8b8db14922eb0724e9cd62f4
Miguel Villarica <mamvillarica@gmail.com>
2025-04-23 22:54:58 +0800

    Added options for changing visual style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import mediapipe as mp
 import cv2
+import numpy as np
 
 # Face Mesh
 mp_face_mesh = mp.solutions.face_mesh
@@ -12,6 +13,30 @@ hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_c
 #Drawing Utils
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
+
+# Drawing Specifications
+face_mesh_drawing_spec = mp_drawing.DrawingSpec(
+    color=(0, 255, 0),  # Green color in BGR
+    thickness=1,
+    circle_radius=1
+)
+
+face_mesh_connection_spec = mp_drawing.DrawingSpec(
+    color=(255, 0, 0),  # Blue color in BGR 
+    thickness=1
+)
+
+hand_landmark_spec = mp_drawing.DrawingSpec(
+    color=(255, 255, 255),  # White color in BGR
+    thickness=2,
+    circle_radius=2
+)
+
+hand_connection_spec = mp_drawing.DrawingSpec(
+    color=(255, 255, 255),  # White color in BGR
+    thickness=2
+)
+
 
 #Video Capture (only works with webcam)
 cap = cv2.VideoCapture(0)
@@ -30,6 +55,7 @@ while cap.isOpened():
     target_height = 720
     target_width = int(width * (target_height / height))
     frame = cv2.resize(frame, (target_width, target_height))
+    frame = cv2.flip(frame, 1)
     # changing color space for compatibility with mediapipe
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     
@@ -42,6 +68,8 @@ while cap.isOpened():
                 image=frame,
                 landmark_list=face_landmarks,
                 connections=mp_face_mesh.FACEMESH_TESSELATION,
+                landmark_drawing_spec=face_mesh_drawing_spec,
+                connection_drawing_spec=face_mesh_connection_spec
             )
     
     #mediapipe processing, hands
@@ -49,11 +77,16 @@ while cap.isOpened():
 
     if hands_results.multi_hand_landmarks:
         for hand_landmarks in hands_results.multi_hand_landmarks:
+            #! drawing landmarks and connections
             mp_drawing.draw_landmarks(
                 image=frame,
                 landmark_list=hand_landmarks,
-                connections=mp_hands.HAND_CONNECTIONS
+                connections=mp_hands.HAND_CONNECTIONS,
+                landmark_drawing_spec=hand_landmark_spec,
+                connection_drawing_spec=hand_connection_spec
             )
+
+
 
     # Display the frame
     cv2.imshow('MediaPipe Face Mesh', frame)
